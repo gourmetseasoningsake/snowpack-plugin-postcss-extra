16aeab6fe635ea17e3ec6ce3e5766d10ab7cbe3b
gourmetseasoningsake <jerome@shtill.com>
2021-02-05 21:10:47 +0100

    add bundler

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import pkg from './package.json';
+
+
+
+export default [
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  { input: 'plugin.js',
+    output: [
+        { file: pkg.main,
+          format: 'cjs',
+          exports: 'default'
+        },
+        { file: pkg.module,
+          format: 'es',
+          exports: 'default'
+        }
+    ],
+
+    //specifies the extensions of files that the plugin will operate on
+    plugins: [ nodeResolve({ extensions: ['.js' ] }) ],
+    external: [
+      'path',
+      'glob',
+      'execa',
+      'crocks/List',
+      'crocks/Async',
+      'crocks/helpers/curry',
+      'crocks/helpers/liftA2',
+      'crocks/helpers/liftA3',
+      'crocks/Maybe/safe',
+      'crocks/Maybe/getProp',
+      'crocks/predicates/isSame',
+      'crocks/predicates/isObject',
+      'crocks/predicates/isString',
+      'crocks/predicates/isTruthy',
+      'crocks/combinators/constant',
+    ],
+  }
+]
